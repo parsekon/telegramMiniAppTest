8773c69c8d2e97de99a228b34c5681678bf3bef4
DimaProCrypto <budanovdm@yahoo.com>
2024-07-14 01:18:54 -0700

    Css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import{ useEffect } from "react";
 
-const tg = window.Telegram.WebApp;
+
 
 function App() {
 
@@ -9,14 +9,9 @@ function App() {
     tg.ready();
   })
 
-  const onClose = () => {
-    tg.close();
-  }
-
   return (
     <div className="App">
     work!
-    <button onClick={ onClose }>Закрыть</button>
     </div>
   );
 }
