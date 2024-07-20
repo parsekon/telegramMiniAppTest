86f8c2c81e6a04a304cb0a3cc682c0089e854e3f
DimaProCrypto <budanovdm@yahoo.com>
2024-07-19 23:00:30 -0700

    Click main button

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useTelegram } from '../hooks/useTelegram';
 import './Form.css';
 
@@ -8,6 +8,22 @@ const Form = () => {
   const [subject, setSubject] = useState('physical');
 
   const {tg} = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+      country,
+      street,
+      subject
+    }
+    tg.sendData(JSON.stringify(data));
+  }, []);
+
+  useEffect(() => {
+    tg.WebApp.onEvent('mainButtonClicked', onSendData); 
+    return () => {
+      tg.WebApp.offEvent('mainButtonClicked', onSendData);
+    }
+  }, []);
 
   useEffect(() => {
     tg.MainButton.setParams({
