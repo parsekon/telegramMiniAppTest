5801d43ea1334aa88b6f33ccf6623425ab0fd15e
DimaProCrypto <budanovdm@yahoo.com>
2024-07-23 23:38:48 -0700

    ...

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
-import {useTelegram} from '../hooks/useTelegram';
-import {useCallback, useEffect} from 'react';
+import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Jeanse', price: 50, description: 'Blue, skin'},
-    {id: '2', title: 'Jacket', price: 500, description: 'Green, warm'},
-    {id: '3', title: 'Short', price: 60, description: 'Blue, skin'},
-    {id: '4', title: 'T-Short', price: 30, description: 'Blue, skin'},
-    {id: '5', title: 'Pans', price: 20, description: 'Green, warm'},
-    {id: '6', title: 'Hat', price: 80, description: 'Blue, skin'},
-    {id: '7', title: 'Sneakers', price: 100, description: 'Blue, skin'},
-    {id: '8', title: 'Suite', price: 450, description: 'Green, warm'}
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
-const getTotalPrice = (items) => {
+const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
         return acc += item.price
-    }, 0);
+    }, 0)
 }
 
 const ProductList = () => {
@@ -29,27 +29,26 @@ const ProductList = () => {
         const data = {
             products: addedItems,
             totalPrice: getTotalPrice(addedItems),
-            queryId, 
+            queryId,
         }
         fetch('http://37.1.199.186:8000/web-data', {
             method: 'POST',
             headers: {
-                'Content-type': 'application/json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(data)
-        });
-      }, []);
-    
-      useEffect(() => {
-          tg.onEvent('mainButtonClicked', onSendData); 
+        })
+    }, [addedItems])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-          tg.offEvent('mainButtonClicked', onSendData);
+            tg.offEvent('mainButtonClicked', onSendData)
         }
-      }, [onSendData]);
+    }, [onSendData])
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
-
         let newItems = [];
 
         if(alreadyAdded) {
@@ -58,29 +57,29 @@ const ProductList = () => {
             newItems = [...addedItems, product];
         }
 
-        setAddedItems(newItems);
+        setAddedItems(newItems)
 
         if(newItems.length === 0) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Buy ${getTotalPrice(newItems)}` 
+                text: `Купить ${getTotalPrice(newItems)}`
             })
         }
     }
 
-    return(
+    return (
         <div className={'list'}>
-            {products.map(item => {
-                return <ProductItem
+            {products.map(item => (
+                <ProductItem
                     product={item}
                     onAdd={onAdd}
                     className={'item'}
                 />
-            })}
+            ))}
         </div>
     );
-}
+};
 
 export default ProductList;
